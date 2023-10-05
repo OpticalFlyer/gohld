54952cf05760ef88713f739c599e2ec4cf0cb35f
Chris Bomar <113836739+OpticalFlyer@users.noreply.github.com>
2023-10-05 07:17:57 -0500

    Sites loader and endpoint

---
+++ loader/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type Site struct {
+	ID          uint   `gorm:"primary_key"`
+	SiteID      string `gorm:"column:site_id;type:varchar(100)"`
+	Address     string `gorm:"type:varchar(500)"`
+	Eligibility string `gorm:"type:varchar(500)"`
+	Tier        string `gorm:"type:varchar(500)"`
+	Latitude    float64
+	Longitude   float64
+	AddressType string `gorm:"column:address_type;type:varchar(500)"`
+	Geom        string `gorm:"type:geometry(Point, 4326)"`
+}
+
+func (Site) TableName() string {
+	return "sites"
+}
+
+func main() {
+	db, err := gorm.Open("postgres", "host=localhost user=hld dbname=hld sslmode=disable password=malloc32$")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// AutoMigrate will create the table and add missing columns, indexes
+	db.AutoMigrate(&Site{})
+
+	// Add a GiST index for the Geom column
+	db.Exec("CREATE INDEX idx_sites_geom ON sites USING GIST (geom);")
+
+	// Parse the CSV and insert into the database
+	parseCSVAndInsert(db)
+}
+
+func parseCSVAndInsert(db *gorm.DB) {
+	f, err := os.Open("/Users/chris/Downloads/All Colorado Address Points CPF.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	totalLines := 0
+	for scanner.Scan() {
+		totalLines++
+	}
+	f.Seek(0, io.SeekStart) // Reset file reader to the beginning
+	fmt.Println("Total lines in file:", totalLines)
+
+	r := csv.NewReader(f)
+
+	lineCount := 1
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("Error reading record on line %d: %v", lineCount, err)
+			lineCount++
+			continue
+		}
+
+		lat, err := strconv.ParseFloat(record[4], 64)
+		if err != nil {
+			log.Printf("Error parsing latitude on line %d: %v", lineCount, err)
+			lineCount++
+			continue
+		}
+
+		lon, err := strconv.ParseFloat(record[5], 64)
+		if err != nil {
+			log.Printf("Error parsing longitude on line %d: %v", lineCount, err)
+			lineCount++
+			continue
+		}
+
+		site := Site{
+			SiteID:      record[0],
+			Address:     record[1],
+			Eligibility: record[2],
+			Tier:        record[3],
+			Latitude:    lat,
+			Longitude:   lon,
+			AddressType: record[6],
+			Geom:        fmt.Sprintf("SRID=4326;POINT(%f %f)", lon, lat),
+		}
+		db.Create(&site)
+
+		lineCount++
+		progress := float64(lineCount) / float64(totalLines) * 100.0
+		fmt.Printf("\rProgress: %.2f%%", progress)
+	}
+}
--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/OpticalFlyer/hld/centerlines"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+type PolygonRequest struct {
+	Polygon [][]float64 `json:"polygon"`
+}
 
 func handleGetRoadCenterlineData(c *gin.Context) {
 	var coords struct {
@@ -29,6 +37,61 @@ func handleGetRoadCenterlineData(c *gin.Context) {
 	c.Data(http.StatusOK, "application/json", data)
 }
 
+func handleGetSitesInPolygon(c *gin.Context) {
+	var polyRequest PolygonRequest
+	if err := c.ShouldBindJSON(&polyRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Convert the provided polygon coordinates to WKT (Well-Known Text) format
+	polygonWKT := "POLYGON(("
+	for _, coord := range polyRequest.Polygon {
+		polygonWKT += fmt.Sprintf("%f %f,", coord[1], coord[0]) // Note: PostGIS uses Longitude Latitude ordering
+	}
+	// Close the polygon by adding the first point again
+	polygonWKT += fmt.Sprintf("%f %f", polyRequest.Polygon[0][1], polyRequest.Polygon[0][0])
+	polygonWKT += "))"
+
+	fmt.Println(polygonWKT)
+
+	// Establish your database connection
+	db, err := gorm.Open("postgres", "host=localhost user=hld dbname=hld sslmode=disable password=malloc32$")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to database"})
+		return
+	}
+	defer db.Close()
+	fmt.Println("Connected to database")
+
+	// Fetch points within the polygon and convert to GeoJSON
+	rows, err := db.Raw(`SELECT ST_AsGeoJSON(geom) FROM sites WHERE ST_Within(geom, ST_GeomFromText(?, 4326))`, polygonWKT).Rows()
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data from database"})
+		return
+	}
+	defer rows.Close()
+
+	fmt.Println("Fetched rows")
+
+	// Convert rows to a GeoJSON feature collection
+	featureCollection := map[string]interface{}{
+		"type":     "FeatureCollection",
+		"features": []map[string]interface{}{},
+	}
+
+	for rows.Next() {
+		var geojsonStr string
+		rows.Scan(&geojsonStr)
+		var feature map[string]interface{}
+		json.Unmarshal([]byte(geojsonStr), &feature)
+		featureCollection["features"] = append(featureCollection["features"].([]map[string]interface{}), feature)
+	}
+
+	c.JSON(http.StatusOK, featureCollection)
+}
+
 func main() {
 	r := gin.Default()
 
@@ -40,6 +103,8 @@ func main() {
 	r.Static("/static", "./static")
 
 	r.POST("/road-centerline", handleGetRoadCenterlineData)
+
+	r.POST("/sites-in-polygon", handleGetSitesInPolygon)
 
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
